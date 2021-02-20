84a8ed51dcfb476f51cc5b4081e27e3d769e4760
A.V.S.Chaitanya <chaithanya1996@outlook.com>
2021-02-20 15:26:16 +0530

    Improvements to the DB config interface

--- src/main/scala/me/adapa/dlake/denomalizer/util/DBUtil.scala
+++ src/main/scala/me/adapa/dlake/denomalizer/util/DBUtil.scala
@@ -1,40 +1,73 @@
 package me.adapa.dlake.denomalizer.util
 
 import com.typesafe.config.Config
-import me.adapa.dlake.denomalizer.config.BaseTableNotConfiguredInDB
-import me.adapa.dlake.denomalizer.entities.JoinPathInfo
+import me.adapa.dlake.denomalizer.config.{BadTableConfigException, BaseTableNotConfiguredInDB}
+import me.adapa.dlake.denomalizer.entities.{JoinPathInfo, TableConfigRecord}
+
 import java.sql.DriverManager
+import scala.collection.mutable
+
 
 object DBUtil {
+
+  def parseSelectColumns(pgColArray:Array[String]):List[List[String]] = {
+      pgColArray.map(_.trim.split(",").toList).toList
+  }
+
+  @throws[BadTableConfigException]
+  def parseTableJoinCols(string: String):mutable.Map[String,String] = {
+    val rawStringMaps = string.trim.split(",").map(x => x.trim.split("->"))
+    val filteredRawMaps = for (arrMapSingle <- rawStringMaps if arrMapSingle.size == 2 ) yield arrMapSingle
+    var mappedCols = mutable.HashMap[String, String]()
+    filteredRawMaps.foreach(x => mappedCols+= (x(0)->x(1)))
+    return mappedCols
+  }
 
   @throws[BaseTableNotConfiguredInDB]
   def getAppJoinforTable(basetableName:String,adminConfig:Config):JoinPathInfo = {
     val jdbcQuery =
-      s"SELECT base_table, join_table_list, join_on_col_list, table_column_list FROM public.${adminConfig.getString("admin.join_path_table")};"
+      s"SELECT base_table, join_table_list, join_on_col_list, table_column_list FROM public.${adminConfig.getString("admin.join_path_table")} where base_table='${basetableName}';"
 
       val jCon = DriverManager.getConnection(
         s"jdbc:postgresql://${adminConfig.getString("db.hostname")}:${adminConfig.getString("db.port")}/${adminConfig.getString("db.db")}",
         adminConfig.getString("db.username"),
         adminConfig.getString("db.password"))
 
-
       val preparedSt = jCon.createStatement
-
       val rs = preparedSt.executeQuery(jdbcQuery)
 
     var joinPathInfo:JoinPathInfo = null;
-    if (rs.next()){
+
+   while(rs.next()){
       val colList = if(rs.getString("table_column_list").isEmpty) List[String]() else rs.getString("table_column_list").trim.split(",").toList ;
       joinPathInfo = JoinPathInfo(rs.getString("base_table"),
           rs.getString("join_table_list").trim.split(",").toList,
           rs.getString("join_on_col_list").trim.split(",").toList,
         colList)
-      }else {
-      rs.close()
-      jCon.close()
-      throw  new BaseTableNotConfiguredInDB;
-    }
-
+      }
     joinPathInfo
+  }
+
+  @throws[BaseTableNotConfiguredInDB]
+  def getAppJoinforTable2(basetableName:String,adminConfig:Config):TableConfigRecord  = {
+    val jdbcQuery =
+      s"SELECT base_table, join_table_list, table_column_list FROM public.${adminConfig.getString("admin.join_path_table")} where base_table='${basetableName}';"
+
+    val jCon = DriverManager.getConnection(
+      s"jdbc:postgresql://${adminConfig.getString("db.hostname")}:${adminConfig.getString("db.port")}/${adminConfig.getString("db.db")}",
+      adminConfig.getString("db.username"),
+      adminConfig.getString("db.password"))
+
+    val preparedSt = jCon.createStatement
+    val rs = preparedSt.executeQuery(jdbcQuery)
+
+    var tableConfigRecord:TableConfigRecord = null;
+
+    while(rs.next()){
+      tableConfigRecord = TableConfigRecord(rs.getString("base_table"),
+        parseTableJoinCols(rs.getString("join_table_list")),
+        parseSelectColumns(rs.getArray("table_column_list").getArray.asInstanceOf[Array[String]]))
+    }
+    tableConfigRecord
   }
 }
